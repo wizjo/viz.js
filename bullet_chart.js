45a5437e82b8c78c381a88cf84bbdd396989064d
Jo Pu <zpu@yammer-inc.com>
2012-01-23 14:00:21 -0800

    Adding mouseover events option to BulletChart.

--- bullet_chart.js
+++ bullet_chart.js
@@ -25,7 +25,9 @@ var BulletChart = Chart.extend({
     this.title = this.title || false;
     this.subtitle = this.subtitle || false;
     this.fill = this.fill || d3.scale.category10();
-    
+    this.hover_events = this.hover_events || false;
+    this.hover_label_color = this.hover_label_color || "#333";
+        
     this.chart = this.chart || d3.chart.bullet()
         .width(this.width - this.leftMargin - this.rightMargin)
         .height(this.height - this.topMargin - this.bottomMargin);
@@ -38,10 +40,26 @@ var BulletChart = Chart.extend({
         .attr("height", this.height)
       .append("svg:g")
         .attr("transform", "translate(" + this.leftMargin + ", " + this.topMargin + ")")
-        .call(this.chart);
+        .call(this.chart)
+        .on("mouseover", mouseover);
     
     this.t = d3.selectAll("svg.bullet")
         .attr("text-anchor", "end");
+    
+    this.tip = d3.selectAll("rect.measure");
+    
+    this.hover_idx = -1;
+    
+    // hover over bulletchart to view metric value
+    function mouseover(d, i) {
+      self.hover_idx = i;
+      d3.selectAll("text.tip")
+        .attr("fill", function(d, i) { return self.hover_idx==i? self.hover_label_color : "none"; });
+      d3.selectAll("rect.measure")
+        .attr("stroke", function(d, i) { return self.hover_idx==i? "#FFF" : "none"; })
+        .attr("stroke-width", "2px");
+      self.hover_idx = -1;
+    }
     
     if(this.title) {
       this.addTitle();
@@ -50,21 +68,37 @@ var BulletChart = Chart.extend({
     if(this.subtitle) {
       this.addSubTitle();
     }
+    
+    if(this.hover_events) {
+      this.addChartTips();
+    }
   }
   
   , addTitle: function() {
-    this.t.append("text")
+    this.t.append("svg:text")
         .attr("class", "title")
         .attr("transform", "translate(" + this.leftMargin*.95 + ", " + (this.height - this.topMargin - this.bottomMargin) / 2 + ")")
-        .text(function(d) { return d.title; })
+        .text(function(d) { return d.title; });
   }
   
   , addSubTitle: function() {
-    this.t.append("text")
+    this.t.append("svg:text")
         .attr("class", "subtitle")
         .attr("transform", "translate(" + this.leftMargin*.95 + ", " + (this.height - this.topMargin - this.bottomMargin) / 2 + ")")
         .attr("dy", "1em")
-        .text(function(d) { return d.subtitle; })
+        .text(function(d) { return d.subtitle; });
+  }
+  
+  , addChartTips: function() {
+    this.t.append("svg:text")
+        .attr("class", "tip")
+        .attr("dy", ".8em")
+        .attr("transform", function(d) {
+          return "translate(" + (self.leftMargin + (self.width - self.leftMargin - self.rightMargin) / d.ranges[d.ranges.length - 1] * d.measures[0])
+            + ", " + (self.height - self.topMargin - self.bottomMargin) / 2 + ")";
+        })
+        .text(function(d) { return d.measures[0]; })
+        .attr("fill", "none");
   }
   
 });
