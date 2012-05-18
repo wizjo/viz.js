43347b6c860f74934c9b1b816cff5653d57fc6f4
Justin Berka <jberka@yammer-inc.com>
2012-05-17 19:12:22 -0700

    Asset pipeline manifest

---
+++ manifests/viz.js
@@ -0,0 +1,17 @@
+// Manifest file for viz.js
+//
+//= require viz.js/bar_chart
+//= require viz.js/bullet_chart
+//= require viz.js/chart
+//= require viz.js/class
+//= require viz.js/column_chart
+//= require viz.js/concentric_chart
+//= require viz.js/filled_series
+//= require viz.js/force_directed
+//= require viz.js/gantt_chart
+//= require viz.js/line_chart
+//= require viz.js/pie_chart
+//= require viz.js/spark_bar
+//= require viz.js/spark_line
+//= require viz.js/stream_graph
+//
