925ef035485514f07b065fbbf8fd0aa93c18fca2
Josh Ferguson <jferguson@yammer-inc.com>
2012-06-22 15:15:56 -0700

    attribution

--- class.js
+++ class.js
@@ -1,3 +1,14 @@
+// Based in part on John Resig’s Simple JavaScript Inheritance.
+
+/*
+* Copyright (c) John Resig
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
 // John Resig's simple inheritance. Inspired by base2 and Prototype
 // from http://ejohn.org/blog/simple-javascript-inheritance/#postcomment
 (function(){
